a138979996762a5677e0f7740313279c89fe3346
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-09-06 19:15:52 -0300

    new method window

--- src/seasons.py
+++ src/seasons.py
@@ -1,34 +1,6 @@
 import base as b 
 import datetime as dt 
-import pandas as pd 
-from events import probability_distribuition
 
-def sep_seasons(ds, year = 2013):
-    
-    ds = ds.loc[ds.index.year == year]
-    
-    summer = ds.loc[
-        (ds.index < dt.datetime(year, 3, 21))
-        ]
-    
-    fall = ds.loc[
-        (ds.index > dt.datetime(year, 3, 21) ) &
-        (ds.index < dt.datetime(year, 6, 21))
-        ]
-    
-    winter = ds.loc[
-        (ds.index > dt.datetime(year, 6, 21)) &
-        (ds.index < dt.datetime(year, 9, 23))
-        ]
-    
-    spring = ds.loc[
-        (ds.index > 
-         dt.datetime(year, 9, 23)) &               
-        (ds.index < 
-         dt.datetime(year, 12, 21)) 
-                  ]
-    
-    return [spring, fall, winter, summer]
 
 
 def get_doy(dn):
@@ -71,49 +43,31 @@ def spring(df):
     return df.loc[cond]
 
 
-import matplotlib.pyplot as plt 
 
 
-fig, ax = plt.subplots(
-    dpi = 300,
-    ncols = 2, 
-    nrows = 2,
-    sharex = True,
-    sharey = True,
-    figsize = (10, 10)
-    )
 
-plt.subplots_adjust(wspace = 0.1)
+
+
+def season(df, month = 6):
+        
+    dn =  dt.date(2013, month, 21)
+    
+    if month == 12:
+    
+        df['doy'] = df['doy'].where(
+            df['doy'] >= 36, df['doy'] + 365)
+    
+        
+    cond = ((df['doy'] > get_doy(dn) - 45) &
+            (df['doy'] <= get_doy(dn) + 45))
+    
+    return df.loc[cond]
+
 
 df = b.load('all_results.txt')
 
-df = df.loc[df['kp_max'] > 4]
+df['doy'] =  df.index.day_of_year
 
 
 
-df['doy'] = df.index.day_of_year 
-seasons = [summer(df), fall(df), winter(df), spring(df)]
-names = ['summer', 'fall', 'winter', 'spring']
-for i, ax  in enumerate(ax.flat):
 
-    ds = probability_distribuition(
-            seasons[i],
-            step = 0.2, 
-            col_gama = 'all',
-            col_epbs = 'epb'
-            )
-    
-    ax.plot(ds['start'], ds['rate'])
-    ax.set(xlim = [0, 3], 
-           ylim = [-20, 120],
-           title = names[i],
-           )
-    
-    if i >= 2:
-        ax.set(xlabel = '$\\gamma_{FT}~\\times 10^{-3}$ ($s^{-1}$)')
-        
-    if i == 0 or i == 2:
-        ax.set(ylabel = 'EPBs probability \noccurrence ($\\%$)')
-    
-    
-fig.suptitle('disturbed days ($kp > 4$)')
