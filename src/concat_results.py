b017b607437655e0f4c9e4a7f05274fa37d412f2
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-09-09 15:44:36 -0300

    path input

--- src/concat_results.py
+++ src/concat_results.py
@@ -3,24 +3,33 @@ import pandas as pd
 
 
 def gamma():
-    df = b.load('database/Results/gamma/saa.txt').dropna()
     
-    df['all'] = df['all'] * 1e3
+    path = 'database/Results/gamma/saa.txt'
+    df = b.load(path).dropna()
     
+    for col in ['all', 
+                'gravity', 
+                'winds']:
+        
+        df[col] = df[col] * 1e3
+   
     df = df[~df.index.duplicated(keep = 'first')]
+    
+    df = df.loc[~(df['all'] > 3.5)]
     
     return df
 
 def indices(df):
-
-    ds = b.load('database/indices/indeces.txt').dropna()
+    path = 'database/indices/indeces.txt'
+    ds = b.load(path).dropna()
     
     return b.sel_dates(ds, df.index[0], df.index[-1])
 
 
 def epbs(col = -40):
     
-    df = b.load('database/epbs/postsunset.txt')
+    path = 'database/epbs/postsunset.txt'
+    df = b.load(path)
     
     df.rename(columns = {'all': 'epb'}, 
               inplace = True)
@@ -34,7 +43,8 @@ def dst(df):
     return b.sel_dates(ds, df.index[0], df.index[-1])
 
 def pre():
-    df = b.load('digisonde/data/PRE/saa/2013_2022.txt')
+    path = 'digisonde/data/PRE/saa/2013_2022.txt'
+    df = b.load(path)
     
     return df
 
@@ -52,9 +62,9 @@ def concat_results():
     
     return pd.concat([g, i, p, e, d], axis = 1)
 
-
-# df = concat_results()
-    
-# df.to_csv('all_results.txt')
+def main():
+    df = concat_results()
+        
+    df.to_csv('all_results.txt')
 
 
