57968b0f325cc1616945833f0fa9a860934bda3a
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-10-17 15:50:32 -0300

    medium solar level

--- src/geo_levels.py
+++ src/geo_levels.py
@@ -21,6 +21,24 @@ def solar_flux_cycles(
     
     return [lower, medium, high]
 
+def medium_solar_level(
+        df, 
+        flux_col = 'f107a',
+        level = 90, 
+     
+        ):
+
+    lower = df.loc[
+        df[flux_col] <= level
+        ]
+    
+  
+    high = df.loc[
+        df[flux_col] >= level
+        ]
+    
+    return [lower, high]
+
 def storm_levels(df):
     
     week = df[(df['dst'] > -50)]
