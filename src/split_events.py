70820c509e0153ed5590d280b51507f445bd0500
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-10-29 14:51:26 -0300

    build data stats for solar and mag events

--- src/split_events.py
+++ src/split_events.py
@@ -1,4 +1,5 @@
 import events as ev 
+import pandas as pd 
 
 df = ev.concat_results('saa')
 
@@ -8,25 +9,67 @@ col = 'gamma'
 solar_level = 86
 
 
-solar_dfs =  ev.solar_levels(
-    df, 
-    level = 86,
-    flux_col = 'f107a'
-    )
 
 
-for df in solar_dfs:
-    epb = len(df.loc[df['epb'] == 1])
-    no_epb = len(df.loc[df['epb'] == 0])
+
+# for df in solar_dfs:
+#     epb = len(df.loc[df['epb'] == 1])
+#     no_epb = len(df.loc[df['epb'] == 0])
+        
     
-    print(epb, no_epb, len(df))
+
+def percent(frac, total):
     
+    return len(frac) / len(total) * 100
+
+def geomanetic_by_solar(
+        df, 
+        percent_like = False
+        ):
     
-solar = ['low', 'high']
-for i, ds in enumerate(solar_dfs):
+    total = df.copy()
     
-    quiet = ds.loc[ds['kp'] <= 3]
-    distu = ds.loc[ds['kp'] > 3]
-    print(solar[i], 'quiet', len(quiet) / len(df) *100)
-    print(solar[i], 'disturded', len(distu) / len(df) *100)
+    out = {
+        'quiet': [], 
+        'disturded': []
+        }
     
+    solar = ['low', 'high']
+    
+    solar_dfs =  ev.solar_levels(
+        df, 
+        level = 86,
+        flux_col = 'f107a'
+        )
+    
+    for i, ds in enumerate(solar_dfs):
+        
+        quiet = ds.loc[ds['kp'] <= 3]
+        disturded = ds.loc[ds['kp'] > 3]
+        
+        if percent_like:
+            
+            for r in out.keys():
+                
+                out[r].append(
+                    percent(vars()[r], total)
+                )
+        else:  
+            
+            for r in out.keys():
+                out[r].append(len(vars()[r]))
+        
+    ds = pd.DataFrame(out, index = solar)
+
+    ds['total'] = ds.sum(axis = 1)
+
+    ds.loc['total',:] = ds.sum(axis=0)
+    
+    return ds
+
+ds = geomanetic_by_solar(
+        df, 
+        percent_like = False
+        )
+
+ds
