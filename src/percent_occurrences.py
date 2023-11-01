ec090e2eac1bf465a61f32b68091feebf1b84d7a
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-11-01 16:11:58 -0300

    moving from split events

--- src/percent_occurrences.py
+++ src/percent_occurrences.py
@@ -1,28 +1,51 @@
+import events as ev 
+import pandas as pd 
 
-import json 
+def percent(frac, total):
+    
+    return len(frac) / len(total) * 100
 
-my_dict = json.load(open('stations.json'))
+def geomanetic_by_solar(
+        df, 
+        percent_like = False
+        ):
+    
+    total = df.copy()
+    
+    out = {
+        'quiet': [], 
+        'disturded': []
+        }
+    
+    solar = ['low', 'high']
+    
+    solar_dfs =  ev.solar_levels(
+        df, 
+        level = 86,
+        flux_col = 'f107a'
+        )
+    
+    for i, ds in enumerate(solar_dfs):
+        
+        quiet = ds.loc[ds['kp'] <= 3]
+        disturded = ds.loc[ds['kp'] > 3]
+        
+        if percent_like:
+            
+            for r in out.keys():
+                
+                out[r].append(
+                    percent(vars()[r], total)
+                )
+        else:  
+            
+            for r in out.keys():
+                out[r].append(len(vars()[r]))
+        
+    ds = pd.DataFrame(out, index = solar)
 
-common_occurrences = set(my_dict['1'])
+    ds['total'] = ds.sum(axis = 1)
 
-# # Iterate through the keys and find common elements
-for key in my_dict:
-    common_occurrences = common_occurrences.intersection(my_dict[key])
-
-# # Convert the result back to a list if needed
-# common_occurrences_list = list(common_occurrences)
-
-# Print the common occurrences
-# ommon_occurrences_list
-
-
-# receivers = ['cesb', 'saga', 'ceeu', 
-#              'paat', 'rnmo', 'past', 
-#              'amco', 'brft', 'pisr']
-
-# common_occurrences
-
-
-# common_occurrences.intersection(my_dict['2'])
-
-common_occurrences
+    ds.loc['total', :] = ds.sum(axis=0)
+    
+    return ds
