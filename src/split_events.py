e4fb290a23fef822184a1af42ab57e96fece478d
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-11-01 16:13:24 -0300

    split array into number equal parts

--- src/split_events.py
+++ src/split_events.py
@@ -3,61 +3,7 @@ import pandas as pd
 import numpy as np 
 
 
-def percent(frac, total):
-    
-    return len(frac) / len(total) * 100
 
-def geomanetic_by_solar(
-        df, 
-        percent_like = False
-        ):
-    
-    total = df.copy()
-    
-    out = {
-        'quiet': [], 
-        'disturded': []
-        }
-    
-    solar = ['low', 'high']
-    
-    solar_dfs =  ev.solar_levels(
-        df, 
-        level = 86,
-        flux_col = 'f107a'
-        )
-    
-    for i, ds in enumerate(solar_dfs):
-        
-        quiet = ds.loc[ds['kp'] <= 3]
-        disturded = ds.loc[ds['kp'] > 3]
-        
-        if percent_like:
-            
-            for r in out.keys():
-                
-                out[r].append(
-                    percent(vars()[r], total)
-                )
-        else:  
-            
-            for r in out.keys():
-                out[r].append(len(vars()[r]))
-        
-    ds = pd.DataFrame(out, index = solar)
-
-    ds['total'] = ds.sum(axis = 1)
-
-    ds.loc['total', :] = ds.sum(axis=0)
-    
-    return ds
-
-
-# ds = geomanetic_by_solar(
-#         df, 
-#         percent_like = False
-#         )
-# 
 
 def monthly_occurences(df, col = 'epb'):
     
@@ -76,11 +22,7 @@ def monthly_occurences(df, col = 'epb'):
     return pd.DataFrame(out, index = months)
 
 
-# df = ev.concat_results('saa')
-# 
-# df = ev.epbs()
- 
-# level = 861
+
 
 def yearly_occurrences(df):
     
@@ -101,3 +43,34 @@ def yearly_occurrences(df):
         
 
 
+def split_in_equal_parts(
+        arr, 
+        parts = 3
+        ):
+    
+    length = len(arr)
+    part_size = length // parts
+    
+                
+    part1 = arr[:part_size]
+    part2 = arr[part_size:2*part_size]
+    part3 = arr[2*part_size:]
+    
+    return part1, part2, part3
+        
+        
+
+def limits_on_parts(df, parts = 2):
+    
+    arr = sorted(df.values)
+    
+    parts = split_in_equal_parts(
+            arr, 
+            parts = 2
+            )
+        
+    return [p[-1] for p in parts if len(p) != 0]
+    
+df = ev.concat_results('saa')
+
+limits_on_parts(df['f107'], parts = 2)
