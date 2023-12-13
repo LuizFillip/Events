b10034720abbbb1ac903f46343bab2320a002a44
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-12-13 07:38:51 -0300

    test probablity forms

---
+++ src/probability_seek.py
@@ -0,0 +1,25 @@
+import core as c 
+
+
+df = c.concat_results('saa')
+
+col = 'gamma'
+
+
+df = c.seasons(df, 'March equinox')
+
+
+df =  c.solar_levels(
+    df, 
+    level = 86,
+    flux_col = 'f107a'
+    )[0]
+
+# print(df.loc[(df[col] > 2) & (df[col] <= 2.2)])
+
+ds = c.probability_distribuition(
+    df,
+    col
+    )
+
+ds
