e59ae74585783285d2781dff22d3f6c9be5bd81d
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-12-17 10:28:33 -0300

    new kword for split the days

--- src/suppression_events.py
+++ src/suppression_events.py
@@ -1,72 +1,55 @@
-from geophysical_indices import INDEX_PATH
 import pandas as pd
-import base as b
-import PlasmaBubbles as pb 
+import core as c
 import numpy as np
 
 
-path = 'database/epbs/events_types.txt'
-
-
-# df = pd.concat(
-#     [b.load(path), 
-#      b.load(INDEX_PATH)], 
-#     axis = 1).dropna()
-
-
-# ds = df.loc[df['kp'] > 3].iloc[:, :5]
-
-ds = b.load(path)
-
-
-        
-        
 def remove_middle(arr):
     middle_index = len(arr) // 2
 
     if len(arr) % 2 == 0:
         
-        arr = np.delete(arr, [middle_index - 1, middle_index])
+        arr = np.delete(
+            arr, [middle_index - 1, middle_index])
     else:
        
         arr = np.delete(arr, middle_index)
 
     return arr
 
-    
-    
-    
-#
 
-def find_supressions(ds):
-    
-    ds['value'] = pb.event_for_all_longs(
-        ds, 
-        value = 1
-        )
-    
-    out = []
-    
-    for i, row in enumerate(ds['value']):
-    
-        if row == 0:
-    
-            lst = ds.iloc[i - 2: i + 3, -1]
-            
-            lst_remo = remove_middle(lst.values)
-            
-            if all(x == 1 for x in lst_remo):
-               
-                out.append(ds.iloc[i, :-1].to_frame().T)
-            else:
-                pass
-    
-    return pd.concat(out)
+def offset(days):
+    if days % 2 == 0:
+        raise('number the days must be odd')
+    else:
+        return days // 2
 
-df = find_supressions(ds)
 
-df_filtered = df[df.apply(
-    lambda row: 
-    all(x == 0 for x in row), axis=1)
-        ]
+def find_supressions(df,  days = 5, col = 'epb'):
+   
+   out = []
+   for i, row in enumerate(df[col]):
+       
+       if row == 0:
+         
+           cond = slice(i - days, i + days + 1)
+           lst = df.iloc[cond][col]
+       
+           lst_remo = remove_middle(lst.values)
+           
+           if all(x == 1 for x in lst_remo):
+               out.append(lst)
     
+   return pd.concat(out).to_frame(col)
+
+
+
+days = 4
+
+
+df = c.concat_results('saa')
+
+ds = find_supressions(df, days = days)
+
+
+def suppression_days(ds):
+    return ds.loc[ds['epb'] == 0]
