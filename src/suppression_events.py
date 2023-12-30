04145e96081cc2b712e0c816de8bfde86dbbd4da
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-12-30 11:16:05 -0300

    find atypical occurrences

--- src/suppression_events.py
+++ src/suppression_events.py
@@ -1,96 +1,54 @@
 import pandas as pd
 import core as c
 import numpy as np
-import PlasmaBubbles as pb 
-import os 
-import base as b 
-import RayleighTaylor as rt
+
 
 
 def remove_middle(arr):
     middle = len(arr) // 2
-
     if len(arr) % 2 == 0:
-        
-        arr = np.delete(
-            arr, [middle - 1, middle])
+        arr = np.concatenate((arr[:middle - 1], arr[middle + 1:]))
     else:
-       
-        arr = np.delete(arr, middle)
-
+        arr = np.concatenate((arr[:middle], arr[middle + 1:]))
     return arr
 
-
-def find_supressions(df, days = 5, col = 'epb'):
-   
-   out = []
-   for i, row in enumerate(df[col]):
-       
-       if row == 0:
-         
-           cond = slice(i - days, i + days + 1)
-           lst = df.iloc[cond][col]
-       
-           lst_remo = remove_middle(lst.values)
-           
-           if all(x == 1 for x in lst_remo):
-               out.append(lst.to_frame(col))
-    
-   return out
-
-def suppression_days(out, col = 'epb'):
-    ds = pd.concat(out)
-    return ds.loc[ds[col] == 0]
-
-def load_base_roti(ds):
+def atypical_occurrences(df, days=4, col='epb', kind=0):
+    """
+    Kind = 0 for suppresion events amd 1 for seeding events
+    """
+    out = []
+    for i, row in enumerate(df[col]):
         
-    start = ds.index[2]
-    end = ds.index[-1]
+        if row == kind:
+          
+            cond = slice(i - days, i + days + 1)
+            lst = df.iloc[cond][col]
+            
+            if len(lst) == (days * 2 + 1):
+                lst_remo = remove_middle(lst.values)
+                
+                if all(x == (1 - kind) for x in lst_remo):
+                    out.append(lst.to_frame(col))
+     
+    return out
+
+def sel_days(
+        out, 
+        col = 'epb', 
+        kind = 0
+        ):
+    ds = pd.concat(out)
+    return ds.loc[ds[col] == kind]
+
+def suppression_days(days = 4, kind = 0):
+
+    df = c.concat_results('saa')
     
-    path = pb.epb_path(
-        start.year, 
-        root = os.getcwd(), 
-        path = 'longs'
-        )
+    lst_days = atypical_occurrences(
+        df, days = days, kind = kind)
     
-    return b.sel_dates(b.load(path), start, end)
-
-def load_base_gamma(ds):
-
-    ds1 = rt.gammas_integrated(
-        rt.FluxTube_dataset(
-            year = 2013, 
-            site = "saa"
-            ))
-    
-    start = ds.index[2]
-    end = ds.index[-1]
-    
-    ds1['gamma'] = ds1['gamma'] * 1e3
-    return b.sel_dates(ds1, start, end)
+    return sel_days(lst_days, kind = kind)#.index
 
 
-df = c.concat_results('saa')
+suppression_days(kind = 0)
 
-ds = find_supressions(df, days = 4)[0]
-import matplotlib.pyplot as plt
-
-fig, ax = plt.subplots(
-    dpi = 300, sharey = True)
-
-b.config_labels()
-
-df = load_base_roti(ds)
-
-df['-50'].plot(ax = ax)
-
-df2 = load_base_gamma(ds)
-
-ax1 = ax.twinx()
-
-df2['gamma'].plot(ax = ax1, color = 'b', lw = 2, ylim = [0, 3])
-
-ax.set(ylabel = 'ROTI (TECU/min)')
-ax1.set(ylabel = '$\\gamma_{RT} ~(s^{-1})$')
-
-# ds
